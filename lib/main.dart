ca055eb50feed4c2f2a18bc1b3529acfc0166b1c
NueL72 <paulsiaga72@gmail.com>
2025-04-09 08:58:31 +0300

    ninja app

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
-  // debugShowCheckedModeBanner: false,
-    home: Home(),
+      // debugShowCheckedModeBanner: false,
+      home: NinjaId(),
     ));
 
-    class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaId extends StatelessWidget {
+  const NinjaId({super.key});
 
-      // const Home({super.key});
-    
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-      appBar: AppBar(
-        title: const Text('hellow worldww'),
-        centerTitle: true, 
+  // const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: const Text(
+            'Ninja Id Card',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // :Colors.white,
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+          // centerTitle: true,
         ),
-      body: Center(
-        // child: Text(
-        //   'hellow ninjas',
-        //   style: TextStyle(
-        //     fontSize: 20.0,
-        //     fontWeight: FontWeight.bold,
-        //     letterSpacing: 2.0,
-        //     color: Colors.grey[600],
-        //   ),
-        // ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )),
+                  SizedBox(height: 10.0,),
+              Text('Emmanuel Paul Siaga',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  )),
 
-        child: Image(
-          image:NetworkImage('https://nuel72.github.io/emmaweb/src/img/emma_1.png'),
-        )
-      )
-      );
-      }
-    }
+                  SizedBox(height: 30.0,),
+
+                   Text('CURRENT NINJA LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )),
+                  SizedBox(height: 10.0,),
+              Text('LEVEL 8',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  )),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Color(0xFFBDBDBD), // Equivalent to Colors.grey[400]
+                ),
+
+                Text(
+                  'paulsiaga72@gmail.com',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 20,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            ],
+
+          ),
+        ));
+  }
+}
